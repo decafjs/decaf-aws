16fa9dbd8505c1eafad720b10646fc49d279d522
Alexandru Lazar <alextkd123@yahoo.com>
2015-04-22 09:18:47 +0300

    [decaf-aws]: initiate repo with Java AWS SDK + initial config

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * AWS Support for Decaf
+ * @author Alex Lazar
+ *
+ * */
+
+ /*global decaf, require */
+
+"use strict";
+
+decaf.extend(exports, {
+    AmazonS3     : require('lib/AmazonS3').AmazonS3
+});
