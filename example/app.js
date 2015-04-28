67bfb1d15e95a5f807992abb08362171e6511582
Michael Schwartz <mykesx@gmail.com>
2015-04-28 09:14:19 -0700

    new Bucket API

--- example/app.js
+++ example/app.js
@@ -10,17 +10,16 @@ var s3 = aws.getS3Client();
 
 console.dir(s3.listBuckets());
 
+s3.use('decafBucketTest');
+
 if (arguments[1] === 'delete') {
-    s3.remove('decaf-bucket-test', 'foo');
+    s3.decafBucketTest.remove('foo');
     console.log('deleted');
 }
 else {
     console.log('creating bucket');
-    var region = s3.createBucket('decaf-bucket-test');
-    console.dir(region);
-
     console.log('uploading bower.json');
-    s3.putFilePublic('decaf-bucket-test', 'foo', 'bower.json');
+    s3.decafBucketTest.putFile('foo', 'bower.json');
 
-    console.log('URL to uploaded oject: ' + s3.getUrl('decaf-bucket-test', 'foo'));
+    console.log('URL to uploaded oject: ' + s3.decafBucketTest.url('foo'));
 }
