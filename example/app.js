4f77c3193f9e72c2cb36417605a4288b44bfd486
Michael Schwartz <mykesx@gmail.com>
2015-04-28 10:23:48 -0700

    make output of example prettier

--- example/app.js
+++ example/app.js
@@ -5,22 +5,25 @@
 var AWS = require('decaf-aws').AWS,
     aws = new AWS();
 
-console.dir(aws.region);
+console.log('Selected region: ' + aws.region);
 var s3 = aws.getS3Client();
 
+console.log('S3 Bucket List:');
 console.dir(s3.listBuckets());
 
+console.log('using S3 bucket: decafBucketTest');
 s3.use('decafBucketTest');
 
 if (arguments[1] === 'delete') {
+    console.log('deleting item foo');
     s3.decafBucketTest.remove('foo');
-    console.log('deleted');
 }
 else {
-    console.log('creating bucket');
+    console.log('creating item foo');
     console.log('uploading bower.json');
     s3.decafBucketTest.putFile('foo', 'bower.json');
 
     console.log('URL to uploaded oject: ' + s3.decafBucketTest.url('foo'));
 }
+console.log('Items in bucket:');
 console.dir(s3.decafBucketTest.listItems());
