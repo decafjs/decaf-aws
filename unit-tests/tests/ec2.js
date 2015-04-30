af06c47bd022cf14b2823acacb3b40559312e448
Michael Schwartz <mykesx@gmail.com>
2015-04-30 07:03:26 -0700

    test ec2 runInstances

--- unit-tests/tests/ec2.js
+++ unit-tests/tests/ec2.js
@@ -4,9 +4,15 @@
 
 var ec2 = aws.getEC2Client();
 
-suite('ec2', function() {
-    console.log('ec2')
-    test('describe instances', function() {
+suite('ec2', function () {
+    test('describe instances', function () {
         console.dir(ec2.describeInstances());
+    });
+    test('run instances', function () {
+        console.dir(ec2.runInstances({
+            amiId        : 'ami-1ecae776',
+            instanceType : 't2.small',
+            keyPairName: 'test-keypair'
+        }));
     })
 });
