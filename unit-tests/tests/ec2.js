88d441a901de6c19d8ef3b5f5ded78b0f7506725
Michael Schwartz <mykesx@gmail.com>
2015-04-30 09:15:49 -0700

    more ec2 features and tests

--- unit-tests/tests/ec2.js
+++ unit-tests/tests/ec2.js
@@ -5,26 +5,30 @@
 var ec2 = aws.getEC2Client();
 
 suite('ec2', function () {
-    test('describe instances', function () {
-        console.dir(ec2.describeInstances());
+    test('describe-security-groups', function() {
+        console.dir(ec2.describeSecurityGroups);
     });
 
-    test('run instances', function () {
-        var instances  = ec2.runInstances({
-                amiId        : 'ami-1ecae776',
-                instanceType : 't2.small',
-                keyPairName  : 'test-keypair'
-            }),
-            instanceId = instances[0].instanceId;
-        console.dir(instances);
-        console.log('waiting for instance ' + instanceId + ' to be running');
-        ec2.waitForInstance(instanceId, 'running');
-        console.log('Instance ' + instanceId + ' is now running');
-
-        console.log('terminating instance ' + instanceId);
-        ec2.terminateInstance(instanceId);
-        console.log('waiting for instance ' + instanceId + ' to be terminated');
-        ec2.waitForInstance(instanceId, 'terminated');
-        console.log('Instance ' + instanceId + ' is now terminated');
-    });
+    //test('describe-instances', function () {
+    //    console.dir(ec2.describeInstances());
+    //});
+    //
+    //test('run-instances', function () {
+    //    var instances  = ec2.runInstances({
+    //            amiId        : 'ami-1ecae776',
+    //            instanceType : 't2.small',
+    //            keyPairName  : 'test-keypair'
+    //        }),
+    //        instanceId = instances[0].instanceId;
+    //    console.dir(instances);
+    //    console.log('waiting for instance ' + instanceId + ' to be running');
+    //    ec2.waitForInstance(instanceId, 'running');
+    //    console.log('Instance ' + instanceId + ' is now running');
+    //
+    //    console.log('terminating instance ' + instanceId);
+    //    ec2.terminateInstance(instanceId);
+    //    console.log('waiting for instance ' + instanceId + ' to be terminated');
+    //    ec2.waitForInstance(instanceId, 'terminated');
+    //    console.log('Instance ' + instanceId + ' is now terminated');
+    //});
 });
