2f01a1b62436ab43b807fcabaf19592de2f1de26
Michael Schwartz <mykesx@gmail.com>
2015-04-30 06:00:00 -0700

    ec2 test in progress

--- unit-tests/tests/s3.js
+++ unit-tests/tests/s3.js
@@ -2,32 +2,29 @@
  * Created by mschwartz on 4/29/15.
  */
 
-(function () {
-    var s3 = aws.getS3Client();
+var s3 = aws.getS3Client();
 
-    suite('s3-list', function () {
-        console.log('S3 Bucket List:');
-        console.dir(s3.listBuckets());
-    });
+suite('s3-list', function () {
+    console.log('S3 Bucket List:');
+    console.dir(s3.listBuckets());
+});
 
-    suite('s3-create', function () {
+suite('s3-create', function () {
 
-        console.log('using S3 bucket: decafBucketTest');
-        s3.use('decafBucketTest');
+    console.log('using S3 bucket: decafBucketTest');
+    s3.use('decafBucketTest');
 
-        console.log('creating item foo');
-        console.log('uploading bower.json');
-        s3.decafBucketTest.putFile('foo', 'bower.json');
+    console.log('creating item foo');
+    console.log('uploading bower.json');
+    s3.decafBucketTest.putFile('foo', 'bower.json');
 
-        console.log('URL to uploaded oject: ' + s3.decafBucketTest.url('foo'));
-        console.log('Items in bucket:');
-        console.dir(s3.decafBucketTest.listItems());
-    });
+    console.log('URL to uploaded oject: ' + s3.decafBucketTest.url('foo'));
+    console.log('Items in bucket:');
+    console.dir(s3.decafBucketTest.listItems());
+});
 
-    suite('s3-remove', function () {
-        console.log('deleting item foo');
-        s3.decafBucketTest.remove('foo');
-        console.dir(s3.decafBucketTest.listItems());
-    });
-
-}());
+suite('s3-remove', function () {
+    console.log('deleting item foo');
+    s3.decafBucketTest.remove('foo');
+    console.dir(s3.decafBucketTest.listItems());
+});
