bf6cf92ead946fe9469cbe0cd650bd68b3e297c6
Michael Schwartz <mykesx@gmail.com>
2015-04-30 08:30:51 -0700

    test ec2 runInstances

--- unit-tests/tests/ec2.js
+++ unit-tests/tests/ec2.js
@@ -9,11 +9,15 @@ suite('ec2', function () {
         console.dir(ec2.describeInstances());
     });
 
-    //test('run instances', function () {
-    //    console.dir(ec2.runInstances({
-    //        amiId        : 'ami-1ecae776',
-    //        instanceType : 't2.small',
-    //        keyPairName: 'test-keypair'
-    //    }));
-    //})
+    test('run instances', function () {
+        var instances = ec2.runInstances({
+            amiId        : 'ami-1ecae776',
+            instanceType : 't2.small',
+            keyPairName: 'test-keypair'
+        });
+        console.dir(instances);
+        console.log('waiting for instance to be running');
+        ec2.waitForInstance(instances[0].instanceId, 'running');
+        console.log('Instance ' + instances[0].instanceId + ' is now running');
+    });
 });
