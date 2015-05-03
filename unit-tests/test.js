ff9513810fb6e9d790b294319819c8ce2ae47369
Michael Schwartz <mykesx@gmail.com>
2015-05-03 07:36:47 -0700

    require Tests instead of unit-tests

--- unit-tests/test.js
+++ unit-tests/test.js
@@ -2,7 +2,7 @@
  * Created by mschwartz on 4/29/15.
  */
 
-var {test_main, suite, test, assert} = require('unit-test');
+var {test_main, suite, test, assert} = require('Tests');
 
 var AWS = require('decaf-aws').AWS,
     aws = new AWS();
