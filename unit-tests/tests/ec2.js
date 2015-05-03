46a40e2277bc187b6d9b5cc044db9ef9fd90cdd1
Michael Schwartz <mykesx@gmail.com>
2015-05-03 07:11:15 -0700

    documentation

--- unit-tests/tests/ec2.js
+++ unit-tests/tests/ec2.js
@@ -5,56 +5,65 @@
 var ec2 = aws.getEC2Client();
 
 suite('ec2', function () {
-    test('describe-security-groups', function() {
+    var groupId,
+        instanceId;
+
+    test('describe-security-groups', function () {
         console.dir(ec2.describeSecurityGroups());
     });
 
-    test('create-security-group', function() {
-        var groupId = ec2.createSecurityGroup('testgroup', {
-            description: 'unit test group',
-            inbound: [
+    test('describe-instances', function () {
+        console.dir(ec2.describeInstances());
+    });
+
+    test('create-security-group', function () {
+        groupId = ec2.createSecurityGroup('testgroup', {
+            description : 'unit test group',
+            inbound     : [
                 {
-                    protocol: 'tcp',
-                    fromPort: 22,
-                    toPort: 22,
-                    ipRange: [
+                    protocol : 'tcp',
+                    fromPort : 22,
+                    toPort   : 22,
+                    ipRange  : [
                         '0.0.0.0/0'
                     ]
                 }
             ],
-            outbound: [
+            outbound    : [
                 {}
             ]
         });
         console.log('security groups');
         console.dir(ec2.describeSecurityGroups());
+    });
 
-        console.log('deleteing security group ' + groupId);
+    test('run-instances', function () {
+        var instances = ec2.runInstances({
+            amiId             : 'ami-d05e75b8',
+            instanceType      : 't2.micro',
+            keyPairName       : 'test-keypair',
+            securityGroupName : 'testgroup'
+        });
+        instanceId = instances[0].instanceId;
+        console.dir(instances);
+        console.log('waiting for instance ' + instanceId + ' to be running');
+        ec2.waitForInstance(instanceId, 'running');
+        console.log('Instance ' + instanceId + ' is now running');
+
+    });
+
+    test('delete-security-group', function () {
+        console.log('deleting security group ' + groupId);
         ec2.removeSecurityGroup(groupId);
         console.log('security groups');
         console.dir(ec2.describeSecurityGroups());
     });
 
-    //test('describe-instances', function () {
-    //    console.dir(ec2.describeInstances());
-    //});
-    //
-    //test('run-instances', function () {
-    //    var instances  = ec2.runInstances({
-    //            amiId        : 'ami-1ecae776',
-    //            instanceType : 't2.small',
-    //            keyPairName  : 'test-keypair'
-    //        }),
-    //        instanceId = instances[0].instanceId;
-    //    console.dir(instances);
-    //    console.log('waiting for instance ' + instanceId + ' to be running');
-    //    ec2.waitForInstance(instanceId, 'running');
-    //    console.log('Instance ' + instanceId + ' is now running');
-    //
-    //    console.log('terminating instance ' + instanceId);
-    //    ec2.terminateInstance(instanceId);
-    //    console.log('waiting for instance ' + instanceId + ' to be terminated');
-    //    ec2.waitForInstance(instanceId, 'terminated');
-    //    console.log('Instance ' + instanceId + ' is now terminated');
-    //});
+    test('terminate-instance', function () {
+        console.log('terminating instance ' + instanceId);
+        ec2.terminateInstance(instanceId);
+        console.log('waiting for instance ' + instanceId + ' to be terminated');
+        ec2.waitForInstance(instanceId, 'terminated');
+        console.log('Instance ' + instanceId + ' is now terminated');
+    });
 });
